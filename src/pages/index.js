71dd02c45240aac70f251855f03b1aa260a3dcca
DHARANIDHARAN200706 <dharanidharanjaidev@gmail.com>
2025-06-16 19:34:04 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+
+import ShoeStore from './prediction/ShoeStore';
+
+export default function HomePage() {
+  return (
+    <div style={{ fontFamily: 'Arial', padding: '20px', textAlign: 'center' }}>
+     
+      <h1>URWISH SHOE STORE</h1>
+      <ShoeStore />
+    </div>
+  );
+}
